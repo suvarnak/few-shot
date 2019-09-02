3cf009d06bedc60d7b52129c1a8be8b240ad6100
Suvarna Kadam <suvarna.kadam@gmail.com>
2019-09-02 21:48:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 PATH = os.path.dirname(os.path.realpath(__file__))
 
-DATA_PATH = "/content/gdrive/My Drive/dataset"
+DATA_PATH = "/content/few-shot/data/miniImageNet/"
 
 EPSILON = 1e-8
 
